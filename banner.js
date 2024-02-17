5b1513dbbd1895239bb76bfee1dccd6fbd83c925
childerssw <119063116+childerssw@users.noreply.github.com>
2024-02-17 10:56:30 -0500

    Adjusted position formatting

--- banner.js
+++ banner.js
@@ -2,12 +2,12 @@ function main(){
   var tl_canvas = document.getElementById('top_left');
   var tl_context = tl_canvas.getContext('2d');
   tl_context.fillStyle = 'rgba(0, 65, 65, 1.0)';
-  tl_context.fillRect(0, 0, 300, 60);
+  tl_context.fillRect(0, 0, 200, 60);
   tl_context.fillStyle = 'rgba(65, 0, 0, 1.0)';
-  tl_context.fillRect(300, 0, 300, 60);
-  tl_context.font = '30pt Georgia';
+  tl_context.fillRect(200, 0, 230, 60);
+  tl_context.font = 'italic 30pt Georgia';
   tl_context.fillStyle = 'rgba(255, 215, 0, 1.0)';
   tl_context.fillText('RoleCall', 10, 40);
   tl_context.fillStyle = 'rgba(192, 192, 192, 1.0)';
-  tl_context.fillText('Team Silver', 310, 40);
+  tl_context.fillText('Team Silver', 210, 40);
 }
