bb62baffb811fe71447180b62832d4b04b52158d
Chris Skeens <cskee004@odu.edu>
2024-11-10 13:44:19 -0500

    Implemented the acitivty create listing. Added drop down lists for environment and difficulty.

---
+++ frontend/app/src/main/java/com/rolecall/CreateListing.java
@@ -0,0 +1,76 @@
+package com.rolecall;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.rolecall.R;
+
+import java.util.ArrayList;
+
+public class CreateListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_listing);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ArrayList<String> arrayEnv = new ArrayList<>();
+        arrayEnv.add("In-person");
+        arrayEnv.add("Digital");
+        ArrayAdapter<String> adapterEnv = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayEnv);
+        adapterEnv.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        Spinner environment_spinner = findViewById(R.id.pref_environment_spinner);
+        environment_spinner.setAdapter(adapterEnv);
+        environment_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(CreateListing.this, "Selected Location: " + item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        ArrayList<String> arrayDiff = new ArrayList<>();
+        arrayDiff.add("First Game");
+        arrayDiff.add("Casual");
+        arrayDiff.add("Intermediate");
+        arrayDiff.add("Advanced");
+        ArrayAdapter<String> adapterDiff = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayDiff);
+        adapterDiff.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        Spinner difficulty_spinner = findViewById(R.id.campaign_difficulty_spinner);
+        difficulty_spinner.setAdapter(adapterDiff);
+        difficulty_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(CreateListing.this, "Selected Difficulty: " + item, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
