1d73d1c401d9eb6c45699d9edb370318d6eb5e37
childerssw <schil010@odu.edu>
2024-11-10 18:59:58 -0500

    Added activity for detailed view of one listing

--- frontend/app/src/main/java/com/rolecall/MainActivity.java
+++ frontend/app/src/main/java/com/rolecall/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rolecall;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void createAccount(View v){
+        startActivity(new Intent(MainActivity.this, ViewListing.class));
+    }
+
+    public void accountLogin(View v){
+        startActivity(new Intent(MainActivity.this, ViewListing.class));
     }
 }
