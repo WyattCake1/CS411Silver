740f49c4d13b4dce5e66a37c6363526e95137ea0
Havelz <berhanclark@yahoo.com>
2024-11-14 14:04:20 -0500

    Modification to the MYSQL users table to better fit the ERD

--- database/init-test.sql
+++ database/init-test.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100)
+  name VARCHAR(100),
+  password VARCHAR(100),
+  email VARCHAR(100)
 );
 
-INSERT INTO users (name) VALUES ('Berhan');
+INSERT INTO users (name,password,email) VALUES ('Berhans','BEESecret','bclar006@gmail.com');
 INSERT INTO users (name) VALUES ('Bradley');
 INSERT INTO users (name) VALUES ('Chris');
 INSERT INTO users (name) VALUES ('Sebastian');
--- frontend/app/src/main/java/com/rolecall/Registrar.java
+++ frontend/app/src/main/java/com/rolecall/Registrar.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
