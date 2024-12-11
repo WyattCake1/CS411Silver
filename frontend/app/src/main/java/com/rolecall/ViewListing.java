fc29d992ce1a1afe4241a0c3668e0b517d771be9
childerssw <schil010@odu.edu>
2024-12-11 11:23:38 -0500

    Format adjustment

--- frontend/app/src/main/java/com/rolecall/ViewListing.java
+++ frontend/app/src/main/java/com/rolecall/ViewListing.java
@@ -36,9 +36,7 @@ public class ViewListing extends AppCompatActivity {
     }
 
     public void backToListings(View v){
-        Intent intent = new Intent(ViewListing.this, UserListingsPage.class);
-        intent.putExtra("userId", userId);
-        startActivity(intent);
+        finish();
     }
 
     public void findMatches(View v){
@@ -59,12 +57,14 @@ public class ViewListing extends AppCompatActivity {
         TextView gameName = findViewById(R.id.game_name_field);
         TextView difficulty = findViewById(R.id.difficulty_field);
         TextView environment = findViewById(R.id.environment_field);
+        TextView day = findViewById(R.id.day_field);
         TextView startTime = findViewById(R.id.start_time_field);
         TextView endTime = findViewById(R.id.end_time_field);
         TextView role = findViewById(R.id.role_field);
         gameName.setText(display.getGameName());
         difficulty.setText(display.getDifficulty());
         environment.setText(display.getEnvironment());
+        day.setText(display.getDay());
         startTime.setText(display.getStartTime());
         endTime.setText(display.getEndTime());
         role.setText(display.getRole().toString());
